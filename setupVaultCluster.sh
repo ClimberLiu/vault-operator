2bc8e355bc4ce8eda3af43ba36182f58fdff3af1
i322669 <michael.liu03@sap.com>
2020-04-21 15:17:07 +0800

    add self signed certificate for vault tls

--- setupVaultCluster.sh
+++ setupVaultCluster.sh
@@ -83,10 +83,56 @@ create_k8s_secret_for_vault_tls()
     SECRET_NAME=vault-server-tls
     TMPDIR=/tmp
 
+    # for the ENG env, use the wildcard tls secrets created during the namespace creation by the jenkins job generic-k8s-environment-create
     wildcard_sect=$(kubectl get secret -n ${VAULT_NS} -o name | grep wildcard | grep -v wildcard.${VAULT_NS})
-    kubectl get ${wildcard_sect} -n ${VAULT_NS} -o jsonpath='{.data.tls\.key}' | base64 -d > ${TMPDIR}/vault.key
-    kubectl get ${wildcard_sect} -n ${VAULT_NS} -o jsonpath='{.data.tls\.crt}' | base64 -d > ${TMPDIR}/vault.crt
-    curl ${SPANETCA_CERT_URL} --output ${TMPDIR}/vault.ca
+    if [[ ${wildcard_sect} != "" ]]
+    then
+        kubectl get ${wildcard_sect} -n ${VAULT_NS} -o jsonpath='{.data.tls\.key}' | base64 -d > ${TMPDIR}/vault.key
+        kubectl get ${wildcard_sect} -n ${VAULT_NS} -o jsonpath='{.data.tls\.crt}' | base64 -d > ${TMPDIR}/vault.crt
+        curl ${SPANETCA_CERT_URL} --output ${TMPDIR}/vault.ca
+    else
+        SERVICE=wildcard
+        CSR_NAME=vault-csr
+        openssl genrsa -out ${TMPDIR}/vault.key 2048
+        cat <<EOF >${TMPDIR}/csr.conf
+[req]
+req_extensions = v3_req
+distinguished_name = req_distinguished_name
+[req_distinguished_name]
+[ v3_req ]
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+extendedKeyUsage = serverAuth
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = ${SERVICE}
+DNS.2 = ${SERVICE}.${VAULT_NS}
+DNS.3 = ${SERVICE}.${VAULT_NS}.svc
+DNS.4 = ${SERVICE}.${VAULT_NS}.svc.cluster.local
+IP.1 = 127.0.0.1
+EOF
+        openssl req -new -key ${TMPDIR}/vault.key -subj "/CN=${SERVICE}.${VAULT_NS}.svc" -out ${TMPDIR}/server.csr -config ${TMPDIR}/csr.conf
+        cat <<EOF >${TMPDIR}/csr.yaml
+apiVersion: certificates.k8s.io/v1beta1
+kind: CertificateSigningRequest
+metadata:
+  name: ${CSR_NAME}
+spec:
+  groups:
+  - system:authenticated
+  request: $(cat ${TMPDIR}/server.csr | base64 | tr -d '\n')
+  usages:
+  - digital signature
+  - key encipherment
+  - server auth
+EOF
+        kubectl create -f ${TMPDIR}/csr.yaml
+        kubectl certificate approve ${CSR_NAME}
+        serverCert=$(kubectl get csr ${CSR_NAME} -o jsonpath='{.status.certificate}')
+        echo "${serverCert}" | openssl base64 -d -A -out ${TMPDIR}/vault.crt
+        kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}' | base64 -d > ${TMPDIR}/vault.ca
+
+    fi
     echo "===>Store the key, cert, and SAPNet_CA into Kubernetes secret ${SECRET_NAME}."
     kubectl create secret generic ${SECRET_NAME} --namespace ${VAULT_NS} --from-file=vault.key=${TMPDIR}/vault.key \
         --from-file=vault.crt=${TMPDIR}/vault.crt --from-file=vault.ca=${TMPDIR}/vault.ca
