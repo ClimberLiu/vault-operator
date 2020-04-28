2def530e43ba1845ff3d6783aafaeb7aa20ef6ea
i322669 <michael.liu03@sap.com>
2020-04-28 17:34:55 +0800

    verified updateIdlVaultAdminHCL.sh in ENG

--- setupVaultCluster.sh
+++ setupVaultCluster.sh
@@ -5,7 +5,8 @@ trap 'cleanup' SIGINT SIGTERM SIGQUIT ERR EXIT
 usage()
 {
 cat <<-EOF
-usage: $0 [-n <consul_namespace>] [-r <consul-helm_release_name>]
+usage: $0 -e <ENVIRONMENT> [-n <consul_namespace>] [-r <consul-helm_release_name>]
+Ex., setupVaultCluster.sh -e ENG
 EOF
 }
 
@@ -287,12 +288,10 @@ create_admin_and_provisioner_token()
         default_max_ttl_vault_config="8760h"
         idl_vault_admin_token_json=/tmp/idl_vault_admin_token_$$.json
 
-        kubectl cp -n ${VAULT_NS} ${parent_dir}/${idl_vault_admin_dir}/config/${idl_vault_admin_policy_name}.hcl ${vault_server}:/tmp/${idl_vault_admin_policy_name}.hcl
-        kubectl cp -n ${VAULT_NS} ${parent_dir}/${idl_vault_admin_dir}/config/${idl_vault_crud_policy_name}.hcl ${vault_server}:/tmp/${idl_vault_crud_policy_name}.hcl
-        kubectl cp -n ${VAULT_NS} ${parent_dir}/${idl_vault_admin_dir}/config/delta.sh ${vault_server}:/tmp/
-        kubectl exec -n ${VAULT_NS} ${vault_server} -- sh -c "export VAULT_TOKEN=${root_token}; vault policy write -tls-skip-verify ${idl_vault_admin_policy_name} /tmp/${idl_vault_admin_policy_name}.hcl; vault policy write -tls-skip-verify ${idl_vault_crud_policy_name} /tmp/${idl_vault_crud_policy_name}.hcl"
+        kubectl cp -n ${VAULT_NS} ${parent_dir}/${idl_vault_admin_dir}/config ${vault_server}:/tmp/config
+        kubectl exec -n ${VAULT_NS} ${vault_server} -- sh -c "export VAULT_TOKEN=${root_token}; vault policy write -tls-skip-verify ${idl_vault_admin_policy_name} /tmp/config/${idl_vault_admin_policy_name}.hcl; vault policy write -tls-skip-verify ${idl_vault_crud_policy_name} /tmp/config/${idl_vault_crud_policy_name}.hcl"
         kubectl exec -n ${VAULT_NS} ${vault_server} -- sh -c "export VAULT_TOKEN=${root_token}; vault token create -tls-skip-verify -policy=${idl_vault_admin_policy_name} -period=${default_max_ttl_vault_config} -format=json 2> /dev/null" > ${idl_vault_admin_token_json}
-        kubectl exec -n ${VAULT_NS} ${vault_server} -- sh -c "export VAULT_TOKEN=${root_token}; sh /tmp/delta.sh"
+        kubectl exec -n ${VAULT_NS} ${vault_server} -- sh -c "export VAULT_TOKEN=${root_token} VAULT_SKIP_VERIFY=true; cd /tmp/config/ && sh delta.sh"
         idl_vault_admin_token=$(jq '.auth.client_token' ${idl_vault_admin_token_json} | tr -d \")
         kubectl create secret generic idl-vault-admin-token --from-literal=token=${idl_vault_admin_token} -n ${VAULT_NS}
     else
@@ -323,6 +322,7 @@ VAULT_HELM_GITURL=https://github.com/hashicorp/vault-helm.git
 VAULT_HELM_VER=v0.3.3
 SPANETCA_CERT_URL=http://aia.pki.co.sap.com/aia/SAPNetCA_G2.crt
 IDL_VAULT_ADMIN_GITURL=https://github.wdf.sap.corp/sfsf-platform-core/hashicorp-vault-local-setup.git
+eflag=0
 while getopts :n:r:d:e: opt
 do
     case "$opt" in
@@ -341,6 +341,7 @@ do
             fi
             ;;
         e)
+            eflag=1
             ENVIRONMENT=$(echo ${OPTARG} | tr a-z A-Z)
             if [[ ${ENVIRONMENT} = "ENG" ]]
             then
@@ -360,8 +361,15 @@ do
     esac
 done
 
+if [[ ${eflag} -eq 0 ]]
+then
+  echo "The -e option is mandatory"
+  usage
+  exit 2
+fi
+
 check_consul_setup
-#check_vault_namespace
+check_vault_namespace
 create_k8s_secret_for_consul_storage_tls
 create_k8s_secret_for_vault_tls
 if [[ ${ENVIRONMENT} = "ENG" ]]
--- updateIdlVaultAdminHCL.sh
+++ updateIdlVaultAdminHCL.sh
@@ -4,8 +4,28 @@ VAULT_NS=vault
 VAULT_RELEASE=vault
 IDL_VAULT_ADMIN_GITURL=https://github.wdf.sap.corp/sfsf-platform-core/hashicorp-vault-local-setup.git
 vault_server=$(kubectl get pods -n ${VAULT_NS} -l app.kubernetes.io/name=vault,component=server --field-selector status.phase=Running -o jsonpath='{.items[0].metadata.name}')
-root_token=$(jq ".root_token" ${INIT_TOKEN_FILE} | tr -d  \")
+root_token=""
 idl_vault_admin_dir=hashicorp-vault-local-setup
+
+usage()
+{
+cat <<-EOF
+usage: $0 [-n <consul_namespace>] [-t <root-token>]
+EOF
+}
+
+while getopts :t: opt
+do
+    case "$opt" in
+        t)
+            root_token=$OPTARG
+            ;;
+        ?)
+            echo "Invalid option: -$OPTARG"
+            usage
+            exit 2
+    esac
+done
 
 if [[ ! -d ../${idl_vault_admin_dir} ]]
 then
@@ -18,10 +38,15 @@ if [[ -d ../${idl_vault_admin_dir} ]]
 then
     current_dir=$(pwd)
     parent_dir=$(dirname ${current_dir})
+    idl_vault_admin_policy_name="idl-vault-admin"
+    idl_vault_crud_policy_name="idl-vault-secrets-crud"
+    default_max_ttl_vault_config="8760h"
     idl_vault_admin_token_json=/tmp/idl_vault_admin_token_$$.json
 
-    kubectl cp -n ${VAULT_NS} ${parent_dir}/${idl_vault_admin_dir} ${vault_server}:/tmp/${idl_vault_admin_dir}
-    kubectl exec -n ${VAULT_NS} ${vault_server} -- sh -c "export VAULT_TOKEN=${root_token}; cd /tmp/${idl_vault_admin_dir}/config/ && sh configureAdmin.sh"
+    kubectl cp -n ${VAULT_NS} ${parent_dir}/${idl_vault_admin_dir}/config ${vault_server}:/tmp/config
+    kubectl exec -n ${VAULT_NS} ${vault_server} -- sh -c "export VAULT_TOKEN=${root_token}; vault policy write -tls-skip-verify ${idl_vault_admin_policy_name} /tmp/config/${idl_vault_admin_policy_name}.hcl; vault policy write -tls-skip-verify ${idl_vault_crud_policy_name} /tmp/config/${idl_vault_crud_policy_name}.hcl"
+    kubectl exec -n ${VAULT_NS} ${vault_server} -- sh -c "export VAULT_TOKEN=${root_token}; vault token create -tls-skip-verify -policy=${idl_vault_admin_policy_name} -period=${default_max_ttl_vault_config} -format=json 2> /dev/null" > ${idl_vault_admin_token_json}
+    kubectl exec -n ${VAULT_NS} ${vault_server} -- sh -c "export VAULT_TOKEN=${root_token} VAULT_SKIP_VERIFY=true; cd /tmp/config/ && sh delta.sh"
     idl_vault_admin_token=$(jq '.auth.client_token' ${idl_vault_admin_token_json} | tr -d \")
     kubectl create secret generic idl-vault-admin-token --from-literal=token=${idl_vault_admin_token} -n ${VAULT_NS} --dry-run --save-config=false -o yaml | kubectl apply -f -
 else
